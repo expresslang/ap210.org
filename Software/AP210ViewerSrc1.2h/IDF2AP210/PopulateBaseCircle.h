d91ee635a5503c905ff9dd2cb8c94344cb4e86cf
Ronald Tse <ronald.tse@ribose.com>
2022-07-27 01:58:55 +0900

    feat: move AP210ViewerSrc1.2h to independent repo, fixes expresslang/AP210Viewer#1

--- Software/AP210ViewerSrc1.2h/IDF2AP210/PopulateBaseCircle.h
+++
@@ -1,8 +0,0 @@
-#ifndef POPULATEBASECIRCLE_H
-#define POPULATEBASECIRCLE_H
-
-double pythagorean_theorem(double a, double b);
-
-void CalculateBaseCircle(double x1, double y1, double x2, double y2, double angle,
-                         double *xc, double *yc, double *radius);
-#endif
--- Software/AP210ViewerSrc1.2h/SCHEMAS/arm.ws
+++
@@ -1,5 +0,0 @@
-# Complex ARM entities
-ANDOR( library_ee_product_definition package )
-ANDOR( library_ee_product_definition packaged_part )
-ANDOR( library_ee_product_definition physical_unit_usage_view )
-
